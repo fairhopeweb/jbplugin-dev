02e2067134f551a58da91eb1c3bc1a54ef0a554b
GitHub Action <action@github.com>
2023-11-21 08:14:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/fairhopeweb/jbplugindev/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/fairhopeweb/jbplugindev/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fairhopeweb.jbplugindev.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
